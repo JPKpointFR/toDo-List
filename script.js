ad44894815bc9653f51428e22af32b5fd1db7709
Adama Cissé <adam.cis345@gmail.com>
2022-10-01 18:20:43 +0200

    todolist

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+ol.innerHTML = localStorage.getItem("list");
 
+form.onsubmit = () => {
+  const li = document.createElement("li");
+  const spanDel = document.createElement("span");
+  spanDel.textContent = " -";
+  spanDel.onclick = () => del(li);
+  li.innerHTML = champ.value + "";
+  li.appendChild(spanDel);
+  ol.appendChild(li);
+  champ.value = "";
+  noTache.style.display = "none";
+
+  localStorage.setItem("list", ol.innerHTML);
+
+  return false;
+  //   e.preventDefault();
+};
+
+const del = (element) => {
+  element.remove();
+
+  //   if (ol.innerHTML == "") {
+  //     noTache.style.display = "block";
+  //   }
+  noTache.style.display = ol.innerHTML === "" ? "block" : "none";
+};
